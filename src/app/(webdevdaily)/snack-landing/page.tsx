7f57e33356ed0e2589ba0deeeabc5544205b9381
Danny Bao <88077806+ponktoku@users.noreply.github.com>
2025-04-26 16:05:05 +1000

    feat: Add link to snack landing page from Home

---
+++ src/app/(webdevdaily)/snack-landing/page.tsx
@@ -0,0 +1,8 @@
+export default function SnackLanding() {
+  return (
+    <main>
+      <div className="bg-pink-400"></div>
+      <h1>Snack Landing Page</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <div className="bg-blue-500 text-lg">
         Hello world! This is a project in Zed. Welcome to the UI playground!
       </div>
+      <h1>Navigate to your desired UI playground</h1>
+
+      <Link href="/snack-landing">Snack Landing Page</Link>
     </main>
   );
 }
