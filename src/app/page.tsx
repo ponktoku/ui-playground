6938318e093b3dcd2c42b88d204f7164553e6c7a
Danny Bao <88077806+ponktoku@users.noreply.github.com>
2025-04-26 15:47:33 +1000

    styles: Add shadcn-ui and tw-animate-css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="bg-red-500">
+      <div className="bg-blue-500 text-lg">
         Hello world! This is a project in Zed. Welcome to the UI playground!
       </div>
     </main>
