e87ea43b11822fcb663e2a9bbf369761e2b44da8
Danny Bao <88077806+ponktoku@users.noreply.github.com>
2025-04-27 15:21:43 +1000

    feat: Complete Snack Landing Page challenge 306

--- src/app/(webdevdaily)/snack-landing/page.tsx
+++ src/app/(webdevdaily)/snack-landing/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function SnackLanding() {
   return (
-    <main className="bg-pink-300 flex items-center justify-center max-w-full max-h-full">
-      <section className=" bg-white my-10 mx-50 rounded shadow-xl p-20 grid grid-cols-2">
-        <div className="">
+    <main className="bg-pink-300 flex flex-col items-center justify-center max-w-full min-h-dvh">
+      <h1 className="font-bold text-4xl">Snack Landing Page #306</h1>
+
+      <section className=" bg-white my-10 mx-50 rounded shadow-lg p-20 grid grid-cols-2 gap-20">
+        <div className="mt-10">
           <h1 className="font-medium text-5xl leading-15 mb-5 tracking-wider">
             Ready? <br /> Snack time!
           </h1>
-          <p className="overflow-auto text-gray-500 text-xl tracking-wide mb-5">
+          <p className="overflow-auto text-gray-500 text-2xl tracking-wide mb-5">
             Delicious donuts baked to perfection. Sweet, fluffy, and ready to
             enjoy. Perfect for snack breaks or sharing with friends.
           </p>
@@ -73,7 +75,7 @@ export default function SnackLanding() {
               <circle cx="75" cy="75" r="5" fill="black" />
             </svg>
             <Image
-              src="/low-calorie-sticker.png"
+              src="/snack-landing-images/low-calorie-sticker.png"
               alt="Picture of low calorie sticker with star in centre"
               height={165}
               width={165}
@@ -81,7 +83,69 @@ export default function SnackLanding() {
           </div>
         </div>
 
-        <div className="">Image</div>
+        <div className="grid grid-cols-2 gap-10">
+          {/* left hand side */}
+          <div className="flex flex-col gap-10">
+            <a
+              href="https://unsplash.com/photos/four-doughnut-on-plate-POM4KxWZcG8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/snack-landing-images/donuts-one.jpg"
+                alt="donut image 1"
+                height={300}
+                width={300}
+                className="rounded-4xl shadow-md shadow-gray-400 object-cover w-[300px] h-[300px] hover:opacity-90"
+              />
+            </a>
+
+            <a
+              href="https://unsplash.com/photos/brown-and-white-doughnuts-on-white-ceramic-plate-EACvtuV2k2E"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/snack-landing-images/donuts-three.jpg"
+                alt="donut image 3"
+                height={300}
+                width={300}
+                className="rounded-4xl shadow-md shadow-gray-400 object-cover w-[300px] h-[300px] hover:opacity-90"
+              />
+            </a>
+          </div>
+
+          {/* right hand side */}
+          <div className="flex flex-col gap-10 mt-20">
+            <a
+              href="https://unsplash.com/photos/a-bunch-of-doughnuts-that-are-on-a-plate-mfGSidToZys"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/snack-landing-images/donuts-two.jpg"
+                alt="donut image 2"
+                height={300}
+                width={300}
+                className="rounded-4xl shadow-md shadow-gray-400 object-cover w-[300px] h-[300px] hover:opacity-90"
+              />
+            </a>
+
+            <a
+              href="https://unsplash.com/photos/white-and-brown-donuts-on-white-surface-A7PwjrNLf1U"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/snack-landing-images/donuts-four.jpg"
+                alt="donut image 4"
+                height={300}
+                width={300}
+                className="rounded-4xl shadow-md shadow-gray-400 object-cover w-[300px] h-[300px] hover:opacity-90"
+              />
+            </a>
+          </div>
+        </div>
       </section>
     </main>
   );
