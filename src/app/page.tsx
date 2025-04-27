d39ace12de17f764b7ad9e58f6b64223ee04f846
Danny Bao <88077806+ponktoku@users.noreply.github.com>
2025-04-27 22:46:46 +1000

    feat: Populate elements and style homepage with cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,40 @@
-import Link from "next/link";
+import { projects } from "@/data/projects";
+import Card from "@/components/card";
 
 export default function Home() {
   return (
-    <main>
-      <div className="bg-blue-500 text-lg">
-        Hello world! This is a project in Zed. Welcome to the UI playground!
-      </div>
-      <h1>Navigate to your desired UI playground</h1>
+    <>
+      <main className="min-h-dvh flex justify-center px-4 sm:px-8 ">
+        <div className="max-w-7xl w-full px-16">
+          <section className="my-8">
+            <h1 className="text-4xl font-bold mb-4 text-teal-400">
+              Ponktoku's UI Playground
+            </h1>
+            <h2 className="text-lg font-semibold italic text-white/90 mb-12">
+              Featuring challenges from WebDevDaily, and more
+            </h2>
+          </section>
 
-      <Link href="/snack-landing">Snack Landing Page</Link>
-    </main>
+          <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {projects.map((project) => (
+              <Card
+                key={project.slug}
+                title={project.title}
+                slug={project.slug}
+                image={project.image}
+                tags={project.tags}
+                createdAt={project.createdAt}
+              />
+            ))}
+          </section>
+        </div>
+      </main>
+
+      <footer className="text-white max-w-7xl w-full flex justify-center border border-white items-center">
+        <div>
+          <p>Footer</p>
+        </div>
+      </footer>
+    </>
   );
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,40 @@
+import { TProject } from "@/lib/types";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Card({
+  slug,
+  title,
+  image,
+  tags,
+  createdAt,
+}: TProject) {
+  return (
+    <div className="">
+      <div>
+        <Link href={slug}>
+          <Image
+            src={image}
+            height={1000}
+            width={1000}
+            alt="UI Playground Image Card"
+            className=" rounded-2xl object-cover overflow-hidden shadow hover:shadow-lg transition mb-4"
+          ></Image>
+          <h2 className="font-semibold text-lg text-white">{title}</h2>
+        </Link>
+        <h3 className="text-sm text-white/70 mb-2">{createdAt}</h3>
+
+        <div className="flex gap-2">
+          {tags?.map((tag) => (
+            <span
+              key={tag}
+              className="hidden hover:opacity-80 hover:cursor-pointer text-white sm:block px-2 py-1 rounded-full text-sm font-semibold leading-snug bg-gradient-to-tl from-teal-950/40 to-teal-600/40 border border-teal-600"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
