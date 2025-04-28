5a2e670754b221d714f9e5b9ed3039b9811351c7
Danny Bao <88077806+ponktoku@users.noreply.github.com>
2025-04-28 17:13:54 +1000

    chore: Remove dummy projects to test layout

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,18 +7,4 @@ export const projects = [
     tags: ["WebDevDaily", "desktop", "light mode"],
     createdAt: "2025-04-27",
   },
-  {
-    slug: "pac-landing",
-    title: "Snack Landing Page Challenge #306",
-    image: "/thumbnails/snack-landing-thumb.png",
-    tags: ["WebDevDaily", "desktop", "light mode"],
-    createdAt: "2025-04-27",
-  },
-  {
-    slug: "asd-landing",
-    title: "Snack Landing Page Challenge #306",
-    image: "/thumbnails/snack-landing-thumb.png",
-    tags: ["WebDevDaily", "desktop", "light mode"],
-    createdAt: "2025-04-27",
-  },
 ];
